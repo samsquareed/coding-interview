de6d3b97493c1fb23f7af4a48d43d08d6db7d1a5
= <=>
2022-03-22 23:56:10 +0530

    remove duplicates from sorted list clean code solution

--- leetcode/linkedlist/83.removeDupFromSorted.java
+++ leetcode/linkedlist/83.removeDupFromSorted.java
@@ -52,3 +52,18 @@ class Solution2 {
     }
 }
 
+// clean looking solution :
+class SolutionCleanCode {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+
+        while (node != null && node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
+            }
+        }
+        return head;
+    }
+}
