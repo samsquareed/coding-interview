53edbe707fd0298055021a869060a097ae9b4c16
= <=>
2022-03-23 23:52:59 +0530

    intersection point in list most efficient

--- leetcode/linkedlist/160.IntersectionPoint.java
+++ leetcode/linkedlist/160.IntersectionPoint.java
@@ -20,3 +20,27 @@ class Solution {
     }
 }
 
+
+class Solution2 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            if (a == null) {
+                a = headB;
+            } else {
+                a = a.next;
+            }
+            if (b == null) {
+                b = headA;
+            } else {
+                b = b.next;
+            }
+        }
+        return a;
+    }
+}
