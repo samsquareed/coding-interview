45bcef10092a454709272d09a96ee8d447dacbe6
= <=>
2022-03-24 21:47:34 +0530

    bit manipulation hack

--- leetcode/linkedlist/1290.binaryToInt.java
+++ leetcode/linkedlist/1290.binaryToInt.java
@@ -19,3 +19,14 @@ class Solution {
         return res;
     }
 }
+
+class Solution1 {
+    public int getDecimalValue(ListNode head) {
+        int res = 0;
+        while (head != null) {
+            res = (res << 1) | head.val;
+            head = head.next;
+        }
+        return res;
+    }
+}
