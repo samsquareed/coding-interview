9783febf4106f258ba8892cfc698221557d1ce77
= <=>
2022-03-25 22:30:52 +0530

    reverse list inplace

--- leetcode/linkedlist/206.reverseList.java
+++ leetcode/linkedlist/206.reverseList.java
@@ -23,3 +23,16 @@ class Solution {
         return result.next;
     }
 }
+
+class Solution1 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null,next=null,curr=head;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
