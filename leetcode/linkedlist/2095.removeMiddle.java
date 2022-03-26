7640f09e4e416107c7914c31772d1d8029b404b0
= <=>
2022-03-26 22:24:27 +0530

    solution using above techniqueue

--- leetcode/linkedlist/2095.removeMiddle.java
+++ leetcode/linkedlist/2095.removeMiddle.java
@@ -19,3 +19,23 @@ class Solution {
 // slow.val = slow.next.val;   copy the value of next node to curr node.
 // slow.next=slow.next.next;  then connect curr.next with next of next 
 // i,e next node is skipped(bcz it's value already copied to curr node).
+
+class Solution1 {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null)
+            return null;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (slow.next == null) {
+            slow = head;
+            slow.next = null;
+        } else {
+            slow.val = slow.next.val;
+            slow.next = slow.next.next;
+        }
+        return head;
+    }
+}
