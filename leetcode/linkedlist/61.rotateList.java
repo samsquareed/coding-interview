f7659e524564ce4b322adc9a199a8c092da6910b
= <=>
2022-03-28 22:29:39 +0530

    simple staright forward way to rotate list

---
+++ leetcode/linkedlist/61.rotateList.java
@@ -0,0 +1,36 @@
+class Solution {
+    
+    public int Length(ListNode head){
+        int len =0;
+        while(head!=null){
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
+    
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null)
+            return head;
+        int len = Length(head);
+        if(k==0)
+            return head;
+        if(k>len)
+            k = k%len;
+        
+        ListNode kNode = head, first=head,prev=head;
+        for(int i=0;i<k-1;i++){
+            kNode = kNode.next;
+        }
+        
+        while(kNode.next!=null){
+            kNode=kNode.next;
+            prev=head;
+            head=head.next;
+        }
+        kNode.next = first;
+        prev.next=null;
+        kNode.next=first;
+        return head;
+    }
+}
